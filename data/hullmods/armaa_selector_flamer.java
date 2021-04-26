8162054fc93f940180d2a932b8a080b8ea5b4543
gomarz <bra2091589@gmail.com>
2021-04-26 08:29:26 -0700

    aleste

---
+++ data/hullmods/armaa_selector_flamer.java
@@ -0,0 +1,14 @@
+package data.hullmods;
+
+import com.fs.starfarer.api.combat.ShipAPI;
+import com.fs.starfarer.api.combat.BaseHullMod;
+
+public class armaa_selector_flamer extends BaseHullMod {    
+    
+    @Override
+    public String getDescriptionParam(int index, ShipAPI.HullSize hullSize) {
+ 		if (index == 0) return "Plasma Flamer";
+		if (index == 1) return "Remove this hullmod to cycle between weapons.";
+        return null;    
+    }
+}
