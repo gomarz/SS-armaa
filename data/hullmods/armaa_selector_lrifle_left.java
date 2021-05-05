de6719a20e041b6999c89ba9ac3742c5fac0bd08
gomarz <bra2091589@gmail.com>
2021-05-05 06:12:14 -0700

    Aleste

--- data/hullmods/armaa_selector_lrifle_left.java
+++ data/hullmods/armaa_selector_lrifle_left.java
@@ -3,11 +3,11 @@ package data.hullmods;
 import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.combat.BaseHullMod;
 
-public class armaa_selector_chaingun extends BaseHullMod {    
+public class armaa_selector_lrifle_left extends BaseHullMod {    
     
     @Override
     public String getDescriptionParam(int index, ShipAPI.HullSize hullSize) {
- 		if (index == 0) return "Chaingun";
+ 		if (index == 0) return "Rynex Pulse Rifle";
 		if (index == 1) return "Remove this hullmod to cycle between weapons.";
         return null;    
     }
