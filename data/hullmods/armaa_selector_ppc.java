0681733c02699b5e6d7fe9768638fe1564bb0d11
gomarz <bra2091589@gmail.com>
2021-07-10 20:07:46 -0700

    tweaks

---
+++ data/hullmods/armaa_selector_ppc.java
@@ -0,0 +1,14 @@
+package data.hullmods;
+
+import com.fs.starfarer.api.combat.ShipAPI;
+import com.fs.starfarer.api.combat.BaseHullMod;
+
+public class armaa_selector_ppc extends BaseHullMod {    
+    
+    @Override
+    public String getDescriptionParam(int index, ShipAPI.HullSize hullSize) {
+ 		if (index == 0) return "Particle Cannon";
+		if (index == 1) return "Remove this hullmod to cycle between weapons.";
+        return null;    
+    }
+}
