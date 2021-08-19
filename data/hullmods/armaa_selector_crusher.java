bc6c272ee5309bf960f41ab13ccf64c9b3a6f374
gomarz <bra2091589@gmail.com>
2021-08-18 19:02:22 -0700

    fixes

--- data/hullmods/armaa_selector_crusher.java
+++ data/hullmods/armaa_selector_crusher.java
@@ -3,11 +3,11 @@ package data.hullmods;
 import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.combat.BaseHullMod;
 
-public class armaa_selector_minigun extends BaseHullMod {    
+public class armaa_selector_crusher extends BaseHullMod {    
     
     @Override
     public String getDescriptionParam(int index, ShipAPI.HullSize hullSize) {
- 		if (index == 0) return "SCTR-40";
+ 		if (index == 0) return "Crusher Cannon";
 		if (index == 1) return "Remove this hullmod to cycle between weapons.";
         return null;    
     }
