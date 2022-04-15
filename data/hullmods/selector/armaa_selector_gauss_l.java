f8ef38e9ab168576725817c30dd80ab0d4a1d800
gomarz <bra2091589@gmail.com>
2022-04-15 01:21:34 -0700

    Bugfixes for WINGCOM, AI fixes

--- data/hullmods/selector/armaa_selector_gauss_l.java
+++ data/hullmods/selector/armaa_selector_gauss_l.java
@@ -3,22 +3,20 @@ package data.hullmods.selector;
 import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.combat.BaseHullMod;
 
-public class armaa_selector_crusher extends BaseHullMod {    
-
+public class armaa_selector_gauss_l extends BaseHullMod {    
 	  @Override
     	  public int getDisplaySortOrder() 
 	  {
         	return 2000;
     	  }
-
   	  @Override
   	  public int getDisplayCategoryIndex() 
 	  {
 		return 3;
-    	  }    
+    	  }      
     @Override
     public String getDescriptionParam(int index, ShipAPI.HullSize hullSize) {
- 		if (index == 0) return "Crusher Cannon";
+ 		if (index == 0) return "Gauss";
 		if (index == 1) return "Remove this hullmod to cycle between weapons.";
         return null;    
     }
